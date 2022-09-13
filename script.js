722bab05413a6060467ec71fcb5c58381947819c
AlmirNET <almir.neto369@gmail.com>
2022-09-13 13:17:53 -0300

    Adjustments and styling, in html, css and javascript page 4 corousel.
    Adjustments and styling, in html, css and javascript page 4 corousel.

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
 });
